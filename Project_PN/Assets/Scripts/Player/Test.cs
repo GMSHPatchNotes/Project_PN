1bc01cbbcd5bfb118225fdc53333104003999bc8
DDengriL <ddengril@gmail.com>
2023-05-18 18:20:57 +0900

    1

---
+++ Project_PN/Assets/Scripts/Player/Test.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    
+
+
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    void Update()
+    {
+        Debug.Log("Update");
+    }
+}
